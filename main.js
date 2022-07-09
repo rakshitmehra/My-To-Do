617f3ceec029ce544a41993ca3f7ffeeef01082a
Rakshit Mehra <rakshitmehra88@gmail.com>
2022-07-10 00:21:57 +0530

    My-To-Do

---
+++ main.js
@@ -0,0 +1,28 @@
+if (window.localStorage.getItem('id') == undefined) {
+	window.localStorage.setItem('id',1);
+}
+function send(){
+	var task = document.getElementById("task").value;
+	var des = document.getElementById("des").value;
+	var id = window.localStorage.getItem("id");
+	if(task==""||des=="")
+	window.localStorage.setItem("task"+ id, task);
+	window.localStorage.setItem("des"+ id, des);
+	compo(task,des,id);
+	id++;
+	window.localStorage.setItem("id",id);
+
+}
+function compo(task,des,id){
+	var div = document.createElement("div");
+	var h2 = document.createElement("h2");
+	var hr = document.createElement("hr");
+	var p = document.createElement("p");
+	var button = document.createElement("button");
+	h2.innerHTML = task;
+	p.innerHTML = des;
+	button.innerHTML = "Completed";
+	div.id = id;
+	div.append(h2,hr,p,button);
+	document.getElementById("components").appendChild(div);
+}
